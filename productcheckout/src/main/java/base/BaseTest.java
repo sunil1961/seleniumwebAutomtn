435a4e9732e152d11213278c4d06ae9da3005042
sunil <sunilkdas1961@gmail.com>
2025-02-22 19:05:22 +0530

    'chaintestdependancy'

--- productcheckout/src/main/java/base/BaseTest.java
+++ productcheckout/src/main/java/base/BaseTest.java
@@ -1,33 +1,47 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import com.aventstack.chaintest.plugins.ChainTestListener;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
 	public WebDriver driver;
 
+	@Parameters({"browser"})
 	@BeforeMethod
-	public void beforeTest() {
+	public void beforeTest(String browser) {
+		switch (browser) {
+		case "chrome":
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.saucedemo.com/");
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.saucedemo.com/");
+			break;	
 
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.saucedemo.com/");
+		default:
+			break;
+		}
+
+	
 	}
 
 	@AfterMethod
 	public void afterTest() {
 
-		/*
-		 * if (driver != null) { driver.quit();
-		 * 
-		 * }
-		 */
+		
+		  if (driver != null) { 
+			  driver.quit();
+		  
+		  }
+		 
 
 	}
 }
